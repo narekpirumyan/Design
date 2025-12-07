c1dd22f9794029076c00f8fbdb9d72eb0a8055d8
narekpirumyan <pirumyannarek@gmail.com>
2025-12-07 11:16:33 +0100

    Change tutorial overlay back to absolute positioning to keep phone frame visible

--- film-tinder-prototype/src/components/TutorialGuide/TutorialGuide.jsx
+++ film-tinder-prototype/src/components/TutorialGuide/TutorialGuide.jsx
@@ -256,16 +256,16 @@ export function TutorialGuide({
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0"
+            className="absolute inset-0"
             style={{ 
-              position: 'fixed',
+              position: 'absolute',
               top: 0,
               left: 0,
               right: 0,
               bottom: 0,
-              width: '100vw',
-              height: '100vh',
-              zIndex: 9998,
+              width: '100%',
+              height: '100%',
+              zIndex: 1000,
               pointerEvents: 'auto'
             }}
             onClick={(e) => {
